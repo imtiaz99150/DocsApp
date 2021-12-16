5efe4f29871cf48162a2777a39c5c1e45e8ffd6f
M. Haseeb Sheikh <chibi6359@gmail.com>
2021-12-15 17:28:47 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View,SafeAreaView } from 'react-native'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Hello App</Text>
+    </SafeAreaView>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
